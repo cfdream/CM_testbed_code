0e2910bb317c91009b002e3885d9e5c592c9b48d
cfdream <lxuemei3000@gmail.com>
2015-06-21 20:04:29 -0700

    genereate 802.1q packets correctly now

--- pcapFileGenerator/writePacketToPcapFile.h
+++ pcapFileGenerator/writePacketToPcapFile.h
@@ -14,6 +14,23 @@ struct sniff_ethernet {
     u_char ether_dhost[ETHER_ADDR_LEN]; /* Destination host address */
     u_char ether_shost[ETHER_ADDR_LEN]; /* Source host address */
     u_short ether_type; /* IP? ARP? RARP? etc */
+    u_char vlan[2];
+};
+
+/**
+ * IEEE 802.1Q (Virtual Local Area Network) VLAN header, static header
+ * size: 8 bytes
+ */
+struct tcpr_802_1q_hdr
+{
+    uint8_t vlan_dhost[ETHER_ADDR_LEN];  /**< destination ethernet address */
+    uint8_t vlan_shost[ETHER_ADDR_LEN];  /**< source ethernet address */
+    uint16_t vlan_tpi;                   /**< tag protocol ID */
+    uint16_t vlan_priority_c_vid;        /**< priority | VLAN ID */
+#define TCPR_802_1Q_PRIMASK   0x0007    /**< priority mask */
+#define TCPR_802_1Q_CFIMASK   0x0001    /**< CFI mask */
+#define TCPR_802_1Q_VIDMASK   0x0fff    /**< vid mask */
+    uint16_t vlan_len;                   /**< length or type (802.3 / Eth 2) */
 };
 
 /* IP header, big endian */
@@ -75,6 +92,14 @@ u_int get_seqid_of_flow(packet_s* p_packet) {
     return seqid;
 }
 
+/**
+* @brief 
+*
+* @param p_packet
+* @param packet_buff
+*
+* @return pcap len of the packet
+*/
 int generate_one_packet(packet_s* p_packet, char* packet_buff) {
     int payload_len = p_packet->len;
     u_short sport = p_packet->src_port;
@@ -90,9 +115,10 @@ int generate_one_packet(packet_s* p_packet, char* packet_buff) {
     struct sniff_tcp tcp_header;
     struct sniff_ip ip_header;
     struct sniff_ethernet ethernet_header;
+    struct tcpr_802_1q_hdr ethernet_header_vlan;
     memset(&tcp_header, sizeof(tcp_header), 0);
     memset(&ip_header, sizeof(ip_header), 0);
-    memset(&ethernet_header, sizeof(ethernet_header), 0);
+    memset(&ethernet_header_vlan, sizeof(ethernet_header_vlan), 0);
 
     //tcp header
     tcp_header.th_sport = sport;
@@ -108,22 +134,30 @@ int generate_one_packet(packet_s* p_packet, char* packet_buff) {
     ip_header.ip_p = 0x06;  //TCP-0x06, UDP-0x11
     ip_header.ip_src.s_addr = srcip;
     ip_header.ip_dst.s_addr = dstip;
-    
+   
+    /*
     //ethernet header
     strncpy(ethernet_header.ether_dhost, dst_mac, 6);
     strncpy(ethernet_header.ether_shost, src_mac, 6);
-    ethernet_header.ether_type = 0x0800;
+    ethernet_header.ether_type = 0x0081;
+    */
+
+    //ethernet header
+    strncpy(ethernet_header_vlan.vlan_dhost, dst_mac, 6);
+    strncpy(ethernet_header_vlan.vlan_shost, src_mac, 6);
+    ethernet_header_vlan.vlan_tpi = 0x0081;
+    ethernet_header_vlan.vlan_len = 0x0008;
 
     //printf("tcp header size:%u, ip header size:%u, ether header size:%u\n",
-    //    sizeof(tcp_header), sizeof(ip_header), sizeof(ethernet_header));
-    //printf("src_mac:%s, afterCopy:%s\n", src_mac, ethernet_header.ether_shost);
+    //    sizeof(tcp_header), sizeof(ip_header), sizeof(ethernet_header_vlan));
+    //printf("src_mac:%s, afterCopy:%s\n", src_mac, ethernet_header_vlan.ether_shost);
 
-    memset(packet_buff, sizeof(ethernet_header) + sizeof(ip_header) + sizeof(tcp_header)+payload_len, 0);
-    memcpy(packet_buff, &ethernet_header, sizeof(ethernet_header));
-    memcpy(packet_buff+sizeof(ethernet_header), &ip_header, sizeof(ip_header));
-    memcpy(packet_buff+sizeof(ethernet_header)+sizeof(ip_header), &tcp_header, sizeof(tcp_header));
+    memset(packet_buff, sizeof(ethernet_header_vlan) + sizeof(ip_header) + sizeof(tcp_header)+payload_len, 0);
+    memcpy(packet_buff, &ethernet_header_vlan, sizeof(ethernet_header_vlan));
+    memcpy(packet_buff+sizeof(ethernet_header_vlan), &ip_header, sizeof(ip_header));
+    memcpy(packet_buff+sizeof(ethernet_header_vlan)+sizeof(ip_header), &tcp_header, sizeof(tcp_header));
 
-    return sizeof(ethernet_header) + sizeof(ip_header) + sizeof(tcp_header)+payload_len;
+    return sizeof(ethernet_header_vlan) + sizeof(ip_header) + sizeof(tcp_header);
 }
 
 int init_generate_pcpa_file(const char* out_pcap_fname) {
@@ -152,17 +186,17 @@ void close_generate_pcpa_file() {
 
 void generate_one_pcap_pkt(packet_s* p_packet) {
     struct pcap_pkthdr pcap_header;
-    int pkt_len = 0;
+    int pkt_pcap_len = 0;
     char* packet_buffer = (char*) malloc(5000);
 
     /* generate a packet*/
-    pkt_len = generate_one_packet(p_packet, packet_buffer);
+    pkt_pcap_len = generate_one_packet(p_packet, packet_buffer);
 
     /* write packet_buffer to savefile */
     pcap_header.ts.tv_sec = 0xAA779F47;
     pcap_header.ts.tv_usec = 0x90A20400;
-    pcap_header.caplen = pkt_len;
-    pcap_header.len = pkt_len;
+    pcap_header.caplen = pkt_pcap_len;
+    pcap_header.len = pkt_pcap_len + p_packet->len;
 
     pcap_dump(pdumper, &pcap_header, packet_buffer);
     
@@ -175,7 +209,7 @@ void generate_one_pcap_pkt(packet_s* p_packet) {
 //    struct pcap_pkthdr pcap_header;
 //
 //    int i = 0;
-//    int pkt_len = 0;
+//    int pkt_pcap_len = 0;
 //
 //    pd = pcap_open_dead(DLT_EN10MB, 65535 /* snaplen */);
 //
@@ -192,13 +226,13 @@ void generate_one_pcap_pkt(packet_s* p_packet) {
 //         **/
 //
 //        /* generate a packet*/
-//        pkt_len = generate_one_packet(packet_buffer);
+//        pkt_pcap_len = generate_one_packet(packet_buffer);
 //
 //        /* write packet_buffer to savefile */
 //        pcap_header.ts.tv_sec = 0xAA779F47;
 //        pcap_header.ts.tv_usec = 0x90A20400;
-//        pcap_header.caplen = pkt_len;
-//        pcap_header.len = pkt_len;
+//        pcap_header.caplen = pkt_pcap_len;
+//        pcap_header.len = pkt_pcap_len;
 //
 //        pcap_dump(pdumper, &pcap_header, packet_buffer);
 //    }
