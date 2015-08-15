7c72e491677f8f2034f448afc55da93d05461114
cfdream <lxuemei3000@gmail.com>
2015-08-15 16:15:02 +0000

    mininet_script: update prepare_caida_data.py

--- mininet_script/prepare_caida_data.py
+++ mininet_script/prepare_caida_data.py
@@ -4,13 +4,13 @@ import commands
 import sys
 
 class CaidaData():
-    NUM_FILE_PER_HOST = 1
+    NUM_FILE_PER_HOST = 3
     NUM_HOSTS=12
     START_FILE_NO=130000
     DELTA_FILE_NO=100
 
     def process(self, s_host_idx, e_host_idx):
-        fileno = CaidaData.START_FILE_NO-CaidaData.DELTA_FILE_NO+(s_host_idx-1)*CaidaData.NUM_FILE_PER_HOST
+        fileno = CaidaData.START_FILE_NO-CaidaData.DELTA_FILE_NO+(s_host_idx-1)*CaidaData.NUM_FILE_PER_HOST*CaidaData.DELTA_FILE_NO
         for id_host in range(s_host_idx, e_host_idx+1):
             #for each host
             filenames=[]
@@ -32,26 +32,26 @@ class CaidaData():
 
                 filenames.append(filename)
             #cat all extracted files into one file
-            concated_file = "h{0}.concated.csv" .format(id_host)
-            for filename in filenames:
-                concate_cmd = "cat {0}.csv >> {1}" .format(filename, concated_file)
-                commands.getstatusoutput(concate_cmd)
-            print "SUCC: concatate"
+            #concated_file = "h{0}.concated.csv" .format(id_host)
+            #for filename in filenames:
+            #    concate_cmd = "cat {0}.csv >> {1}" .format(filename, concated_file)
+            #    commands.getstatusoutput(concate_cmd)
+            #print "SUCC: concatate"
 
             #generate pcap file from the concatated file
-            pcap_file="h{0}.pcap" .format(id_host)
-            pcap_cmd="../CM_testbed_code/pcapFileGenerator/pcapFileGenerator {0} {1} {2}" .format(id_host, concated_file, pcap_file)
-            commands.getstatusoutput(pcap_cmd)
-            print "SUCC: {0}" .format(pcap_cmd)
+            #pcap_file="h{0}.pcap" .format(id_host)
+            #pcap_cmd="../CM_testbed_code/pcapFileGenerator/pcapFileGenerator {0} {1} {2}" .format(id_host, concated_file, pcap_file)
+            #commands.getstatusoutput(pcap_cmd)
+            #print "SUCC: {0}" .format(pcap_cmd)
 
             #remove middle temp files
             for filename in filenames:
                 remove_cmd = "rm {0}.csv" .format(filename)
                 commands.getstatusoutput(remove_cmd)
-                remove_cmd = "rm {0}.gz" .format(filename)
-                commands.getstatusoutput(remove_cmd)
-            commands.getstatusoutput("rm {0}" .format(concated_file))
-            print "remove succ"
+                #remove_cmd = "rm {0}.gz" .format(filename)
+                #commands.getstatusoutput(remove_cmd)
+            #commands.getstatusoutput("rm {0}" .format(concated_file))
+            #print "remove succ"
 
 if len(sys.argv) !=3:
     print "usage: python prepare_caida_data.py s_host_idx[0-12] e_host_idx[0-12]\n"
